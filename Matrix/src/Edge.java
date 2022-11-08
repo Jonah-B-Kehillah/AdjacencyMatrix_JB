eb3264c9d131f69c2767ed86c2bd0cd4aab93dc0
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-11-07 21:53:57 -0800

    Improved consistency in formatting

--- Matrix/src/Edge.java
+++ Matrix/src/Edge.java
@@ -1,9 +1,9 @@
 public class Edge {
 	public final Node start;
 	public final Node end;
-	public Edge(Node head, Node tail) {
-		this.start = head;
-		this.end = tail;
+	public Edge(Node _head, Node _tail) {
+		this.start = _head;
+		this.end = _tail;
 	}
 	public String toString(){
 		return start+"->"+end;
--- Matrix/src/Node.java
+++ Matrix/src/Node.java
@@ -1,9 +1,20 @@
 public class Node {
 	public final int value;
-	public Node(int _value) {
+	public final int order;
+	
+	/**
+	 * The constructor for the Node() class
+	 * @param _value The value contained in the node
+	 * @param _order The location of the Node in a matrix representation
+	 */
+	public Node(int _value, int _order) {
 		value = _value;
+		order = _order;
 	}
 	public String toString(){
 		return Integer.toString(value);
+	}
+	public boolean equals(Node n){
+		return this.value == n.value && this.order == n.order;
 	}
 }
